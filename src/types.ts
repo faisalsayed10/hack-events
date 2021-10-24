a5093e240b925efb5e25e34cccf2dd073ab1562f
faisalsayed10 <faisal.sayed502@gmail.com>
2021-10-24 19:04:53 +0530

    :label: (event) added types for events

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+export type Event = {
+	id: string;
+	slug: string;
+	title: string;
+	desc: string;
+	leader: string;
+	cal: string;
+	start: string;
+	end: string;
+	youtube?: string;
+	ama: boolean;
+	amaForm: boolean;
+	amaId: string;
+	amaAvatar?: string;
+	avatar: string;
+};
