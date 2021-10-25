59112f47dcf1a59d494c6562639e1fb4755c2d06
faisalsayed10 <faisal.sayed502@gmail.com>
2021-10-25 21:31:50 +0530

    :bug: (pagination) fix and complete pagination for events

--- src/blocks/pagination.ts
+++ src/blocks/pagination.ts
@@ -1,20 +1,20 @@
-export default (start: number, end: number) => [
+export default (start: number, end: number, total: number) => [
   { type: "divider" },
   {
     type: "actions",
     elements: [
-      {
+      ...(start > 0 ? [{
         type: "button",
         text: { type: "plain_text", text: "👈 Previous Page", emoji: true },
         action_id: "prev",
-        value: `${start}, ${end}`,
-      },
-      {
+        value: `${start}`,
+      }] : []),
+      ...(total > end ? [{
         type: "button",
         text: { type: "plain_text", emoji: true, text: "👉 Next Page" },
         action_id: "next",
-        value: `${start}, ${end}`,
-      },
+        value: `${end}`,
+      }] : []),
     ],
   },
 ];
