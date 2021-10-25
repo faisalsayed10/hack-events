73a0353df4047de3c209535a04ff7423463bc56a
faisalsayed10 <faisal.sayed502@gmail.com>
2021-10-25 21:31:29 +0530

    :sparkles: (updateView) update view on filter using common function

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import { App } from "@slack/bolt";
 import axios from "axios";
 import dayjs from "dayjs";
 import Stump from "stump.js";
-import { Event } from "./types";
 import eventsList from "./blocks/events-list";
+import { Event } from "./types";
+import updateView from "./updateView";
 require("dotenv").config();
 
 const PORT = parseInt(process.env.PORT || "5000");
@@ -35,51 +36,31 @@ app.event("app_home_opened", async ({ event, client }) => {
 app.action("select_event", async ({ ack, body, client }) => {
   await ack();
   const event_type = (body as any).actions[0].selected_option.value;
-
-  try {
-    if (event_type === "upcoming") {
-      const { data } = await axios.get<Event[]>(`https://events.hackclub.com/api/events/${event_type}`);
-      const events = data.filter((event) => dayjs(event.start) > dayjs());
-
-      await client.views.update({
-        view_id: (body as any).view.id,
-        hash: (body as any).view.hash,
-        view: {
-          type: "home",
-          blocks: eventsList(events, "upcoming", 0, 20),
-        },
-      });
-    } else if (event_type === "all") {
-      const { data } = await axios.get<Event[]>(`https://events.hackclub.com/api/events/${event_type}`);
-
-      await client.views.update({
-        view_id: (body as any).view.id,
-        hash: (body as any).view.hash,
-        view: {
-          type: "home",
-          blocks: eventsList(data, "total", 0, 20),
-        },
-      });
-    } else if (event_type === "amas") {
-      const { data } = await axios.get<Event[]>(`https://events.hackclub.com/api/${event_type}`);
-      await client.views.update({
-        view_id: (body as any).view.id,
-        hash: (body as any).view.hash,
-        view: {
-          type: "home",
-          blocks: eventsList(data, "ama", 0, 20),
-        },
-      });
-    }
-  } catch (err) {
-    stump.error(err);
-  }
+  await updateView(event_type, client, body, 0, 20);
 });
 
 app.action("ping-me", async ({ ack }) => {
   await ack();
 });
 app.action("cal", async ({ ack }) => await ack());
+
+app.action("prev", async ({ ack, body, client }) => {
+  await ack();
+  const start: number = parseInt((body as any).actions[0].value);
+  const selected_option: string = (body as any).view.blocks.find((block) => block?.accessory?.type === "static_select")
+    .accessory.initial_option.value;
+
+  await updateView(selected_option, client, body, start - 20, start);
+});
+
+app.action("next", async ({ ack, body, client }) => {
+  await ack();
+  const end: number = parseInt((body as any).actions[0].value);
+  const selected_option: string = (body as any).view.blocks.find((block) => block?.accessory?.type === "static_select")
+    .accessory.initial_option.value;
+
+  await updateView(selected_option, client, body, end, end + 20);
+});
 
 (async () => {
   await app.start(PORT);
---
+++ src/updateView.ts
@@ -0,0 +1,44 @@
+import { SlackAction } from "@slack/bolt";
+import { WebClient } from "@slack/web-api";
+import axios from "axios";
+import dayjs from "dayjs";
+import eventsList from "./blocks/events-list";
+import { Event } from "./types";
+
+export default async (type: string, client: WebClient, body: SlackAction, start: number, end: number) => {
+  try {
+    if (type === "upcoming") {
+      const { data } = await axios.get<Event[]>(`https://events.hackclub.com/api/events/${type}`);
+      const events = data.filter((event) => dayjs(event.start) > dayjs());
+
+      await client.views.update({
+        view_id: (body as any).view.id,
+        view: {
+          type: "home",
+          blocks: eventsList(events, "upcoming", start, end),
+        },
+      });
+    } else if (type === "all") {
+      const { data } = await axios.get<Event[]>(`https://events.hackclub.com/api/events/${type}`);
+
+      await client.views.update({
+        view_id: (body as any).view.id,
+        view: {
+          type: "home",
+          blocks: eventsList(data, "total", start, end),
+        },
+      });
+    } else if (type === "amas") {
+      const { data } = await axios.get<Event[]>(`https://events.hackclub.com/api/${type}`);
+      await client.views.update({
+        view_id: (body as any).view.id,
+        view: {
+          type: "home",
+          blocks: eventsList(data, "ama", start, end),
+        },
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
